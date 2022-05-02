4e56d871b24c7c029abfeb7e073f6e83fce18e0c
Need_Not <daltonhornor0@gmail.com>
2022-05-01 17:18:53 -0700

    can't get item to place

--- src/main/java/net/neednot/onewheel/OneWheelItem.java
+++ src/main/java/net/neednot/onewheel/OneWheelItem.java
@@ -2,12 +2,17 @@ package net.neednot.onewheel;
 
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.SpawnEggItem;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.neednot.onewheel.OneWheelEntity;
 
@@ -18,8 +23,15 @@ public class OneWheelItem extends Item {
     }
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+        System.out.println("rn");
+        if (!world.isClient) {
+            System.out.println("in");
+            ServerWorld serverWorld = (ServerWorld) world;
+            BlockPos pos = playerEntity.getCameraBlockPos();
+            OneWheel.OW.spawnFromItemStack(serverWorld, playerEntity.getStackInHand(hand), playerEntity, pos, SpawnReason.SPAWN_EGG, true, false);
+            playerEntity.getStackInHand(hand).setCount(0);
+        }
         return TypedActionResult.success(playerEntity.getStackInHand(hand));
     }
-
 }
 
